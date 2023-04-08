afb482f767ac1a592c35a8848b50b2587ae17b67
ronnie <ronnielin8862@gmail.com>
2023-04-08 23:07:49 +0800

    channel size

--- server/line/msgHandler/process.go
+++ server/line/msgHandler/process.go
@@ -2,12 +2,13 @@ package msgHandler
 
 import (
 	"github.com/line/line-bot-sdk-go/v7/linebot"
+	"line-gpt/server/line/lineUtil"
 	"log"
 )
 
 func ChannelProcessor() {
 	for {
-		event := <-textChannel
+		event := <-lineUtil.TextChannel
 		switch message := event.Message.(type) {
 		case *linebot.TextMessage:
 			textProcess(message.Text, event)
