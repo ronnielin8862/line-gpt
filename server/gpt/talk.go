fa21c4062317de195b5a70a0dd1e4382c80f5042
ronnie <ronnielin8862@gmail.com>
2024-01-01 23:02:23 +0800

    gpt4

--- server/gpt/talk.go
+++ server/gpt/talk.go
@@ -12,7 +12,7 @@ func Talk(ask string) string {
 	resp, err := global.GptClient.CreateChatCompletion(
 		context.Background(),
 		openai.ChatCompletionRequest{
-			Model: openai.GPT3Dot5Turbo,
+			Model: openai.GPT4,
 			Messages: []openai.ChatCompletionMessage{
 				{
 					Role:    openai.ChatMessageRoleUser,
