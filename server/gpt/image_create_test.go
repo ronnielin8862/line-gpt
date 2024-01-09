0cd83088e88346e98e9263a898e0d142965a833c
ronnie <ronnielin8862@gmail.com>
2024-01-09 17:05:57 +0800

    test

--- server/gpt/image_create_test.go
+++ server/gpt/image_create_test.go
@@ -9,7 +9,7 @@ func TestImageCreate(t *testing.T) {
 	config.LoadGlobalConfig()
 	Start()
 
-	ask := "夢幻般的臘腸狗 ya ya"
+	ask := "夢幻般的臘腸狗 ya ya" // test deploy
 	urls := ImageCreate(ask)
 	t.Log(urls)
 }
